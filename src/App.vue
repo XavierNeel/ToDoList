2ff78b55f7eb2ed86da92dd905889f54bf6c8ffb
neel <xavierneel@gmail.com>
2023-05-16 18:51:56 +0200

    html fontion addtodo

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,24 @@
-<template></template>
-<script></script>
-<style></style>
+<template>
+  <div id="app">
+    <Todo></Todo>
+  </div>
+</template>
+
+<script>
+import Todo from "./components/TodoList.vue"
+
+export default {
+  name: 'App',
+  components:{
+    Todo
+  }
+}
+</script>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto%3Awght%40300%3B400%3B700&display=swap');
+
+#app {
+  font-family: 'Roboto', sans-serif;
+}
+
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,64 @@
+<template>
+     <div id = "app">
+            <h1>ToDo's Manager</h1>
+            <div id="ajout-todo"></div>
+            <input type="text" name="newTodo" id="newTodo" v-model="newTodo" @keyup.enter="addTodo"/>
+            <div class="main">
+                <ul class="todoList">
+                    <li class="todo" v-for= "todo in todos">
+                        <label>{{ todo.name }}</label>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            todos: [{
+                name: "tache de test",
+                completed: false
+            }],
+            newTodo: "",
+        }
+
+    },
+    methods: {
+        addTodo () {
+            this.todos.push({
+                completed: false,  
+                name: this.newTodo  
+
+            })
+            this.newTodo= ""
+//     document.getElementById('todos-list').innerHTML = " "
+//     for(let index = 0; index < todos.length; index++) {
+//         let nouvelleBalise = document.createElement('p')
+//         nouvelleBalise.innerHTML = todos[index] + '<input type="submit" value="V" onclick ="removeTodo(' + index + ')">'
+//         document.getElementById('todos-list').appendChild(nouvelleBalise)
+//     }
+ }
+
+// function addTodo() {
+//     todos.push(document.getElementById('newTodo').value)
+//     ecrireTodos()
+// }
+
+// function removeTodo(index) {
+//     todos.splice(index, 1)
+//     ecrireTodos()
+// }
+
+// ecrireTodos()
+
+    }
+}
+
+
+
+</script>
+
+<style></style>
