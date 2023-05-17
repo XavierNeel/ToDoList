40c9efa5d8bbd51588c6f590c30197d7ab8b3553
neel <xavierneel@gmail.com>
2023-05-17 17:56:37 +0200

    nettoyage/checkbox/compteur

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,17 +1,32 @@
 <template>
-     <div id = "app">
-            <h1>ToDo's Manager</h1>
-            <div id="ajout-todo"></div>
-            <input type="text" name="newTodo" id="newTodo" v-model="newTodo" @keyup.enter="addTodo"/>
-            <div class="main">
-                <ul class="todoList">
-                    <li class="todo" v-for= "todo in todos">
-                        <label>{{ todo.name }}</label>
-                    </li>
-                </ul>
-            </div>
-        </div>
 
+    <section class="todoapp">
+        <header class="header">
+            <h1>ToDo's App</h1>
+            <input type="text" name="newTodo" id="newTodo" v-model="newTodo" @keyup.enter="addTodo"/>
+        </header>
+
+        <div class="main">
+            <input type="checkbox" class="toggle-all" v-model="allDone">
+            <ul class="todo-list">
+                <li class="todo" v-for= "todo in filteredTodos" :class="{completed: todo.completed}">
+                    <div class="view">
+                        <input type="checkbox" v-model="todo.completed" class="toggle">
+                        <label>{{ todo.name }}</label>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+    
+    <footer>
+        <span class="todo-count"><strong>{{ remaining }}</strong> tache à faire</span>
+        <ul class="filters">
+            <li><a href="#" :class="{ selected: filter=== 'all' }" @click.prevent="filter = 'all'">total taches</a></li>
+            <li><a href="#" :class="{ selected: filter=== 'todo' }" @click.prevent="filter = 'todo'">total à faire</a></li>
+            <li><a href="#" :class="{ selected: filter=== 'done' }" @click.prevent="filter = 'done'">total faites</a></li>
+        </ul>
+        </footer>
 </template>
 
 <script>
@@ -23,6 +38,7 @@ export default {
                 completed: false
             }],
             newTodo: "",
+            filter: 'all'
         }
 
     },
@@ -30,29 +46,44 @@ export default {
         addTodo () {
             this.todos.push({
                 completed: false,  
-                name: this.newTodo  
-
+                name: this.newTodo
             })
             this.newTodo= ""
-//     document.getElementById('todos-list').innerHTML = " "
-//     for(let index = 0; index < todos.length; index++) {
-//         let nouvelleBalise = document.createElement('p')
-//         nouvelleBalise.innerHTML = todos[index] + '<input type="submit" value="V" onclick ="removeTodo(' + index + ')">'
-//         document.getElementById('todos-list').appendChild(nouvelleBalise)
-//     }
- }
+        }
+    },
+    computed: {
 
-// function addTodo() {
-//     todos.push(document.getElementById('newTodo').value)
-//     ecrireTodos()
-// }
+        allDone: {
+            get () {
+                return this.remaining === 0
 
-// function removeTodo(index) {
-//     todos.splice(index, 1)
-//     ecrireTodos()
-// }
+            },
 
-// ecrireTodos()
+            set (value) {
+                console.log('value',value)
+                this.todos.forEach( todo => {
+                    todo.completed = value
+                    })
+                }
+
+                
+
+            },
+
+        
+        remaining () {
+           return this.todos.filter( todo => !todo.completed).length
+        },
+        filteredTodos () {
+            if (this.filter === 'todo') {
+                return this.todos.filter( todo => !todo.completed)
+            }else if (this.filter === 'done') {
+                return this.todos.filter( todo => todo.completed)
+            }
+                return this.todos
+            
+            
+        }
 
     }
 }
@@ -61,4 +92,5 @@ export default {
 
 </script>
 
-<style></style>
+<style src="./todo.css">
+</style>
